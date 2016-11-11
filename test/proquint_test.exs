9b22e0169db36b281eebd6909d5b2385af85a3db
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-11-11 17:56:05 +0530

    added more tests

--- test/proquint_test.exs
+++ test/proquint_test.exs
@@ -2,15 +2,30 @@ defmodule ProquintTest do
   use ExUnit.Case
   doctest Proquint
 
-  # TODO: add more examples
+  @test_cases [
+    %{ binary: << 63 :: size(8), 84 :: size(8), 220 :: size(8), 193 :: size(8) >>, encoded: "gutih-tugad"},
+    %{ binary: <<127, 0, 0, 1>>, encoded: "lusab-babad"},
+    %{ binary: << 63, 118, 7, 35 >>, encoded: "gutuk-bisog"},
+    %{ binary: << 140, 98, 193, 141 >>, encoded: "mudof-sakat"},
+    %{ binary: << 64, 255, 6, 200 >>, encoded: "haguz-biram"},
+    %{ binary: << 128, 30, 52, 45 >>, encoded: "mabiv-gibot"},
+    %{ binary: << 147, 67, 119, 2 >>, encoded: "natag-lisaf"},
+    %{ binary: << 212, 58, 253, 68 >>, encoded: "tibup-zujah"},
+    %{ binary: << 216, 35, 68, 215 >>, encoded: "tobog-higil"},
+    %{ binary: << 216, 68, 232, 21 >>, encoded: "todah-vobij"},
+    %{ binary: << 198, 81, 129, 136 >>, encoded: "sinid-makam"},
+    %{ binary: << 12, 110, 110, 204 >>, encoded: "budov-kuras"},
+  ]
 
   test "encode" do
-    bin = << 63 :: size(8), 84 :: size(8), 220 :: size(8), 193 :: size(8) >>
-    assert Proquint.encode(bin) == "gutih-tugad"
+    for test_case <- @test_cases do
+      assert Proquint.encode(test_case.binary) == test_case.encoded
+    end
   end
 
   test "decode" do
-    bin = << 63 :: size(8), 84 :: size(8), 220 :: size(8), 193 :: size(8) >>
-    assert Proquint.decode("gutih-tugad") == bin
+    for test_case <- @test_cases do
+      assert Proquint.decode(test_case.encoded) == test_case.binary
+    end
   end
 end
