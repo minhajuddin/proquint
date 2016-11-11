ad6ebf1d0d4399a079d3df164ba57a9daece8cd9
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-11-11 18:05:24 +0530

    version bump

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Proquint.Mixfile do
      docs: [
        extras: ~W(README.md)
      ],
-    version: "1.0.0",
+    version: "1.0.1",
     elixir: "~> 1.3",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
