17eb70e4762f66be5a2a8dff493feb056aa38a5f
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-11-11 17:34:12 +0530

    added license and meta info

--- mix.exs
+++ mix.exs
@@ -3,6 +3,12 @@ defmodule Proquint.Mixfile do
 
   def project do
     [app: :proquint,
+     name: "Proquint",
+     description: description,
+     package: package,
+     docs: [
+       extras: ~W(README.md CHANGELOG.md)
+     ],
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
@@ -27,6 +33,28 @@ defmodule Proquint.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:credo, "~> 0.4", only: [:dev, :test]},
+    ]
+  end
+
+  defp description do
+    """
+    An elixir package that converts ids to proquints,
+    Identifiers that are Readable, Spellable, and Pronounceable.
+    More info about proquints can be found at: https://arxiv.org/html/0901.4016
+    """
+  end
+
+  defp package do
+    [description: description,
+     files: ~w(lib config mix.exs README.md LICENSE),
+     maintainers: ["Khaja Minhajuddin"],
+     licenses: ["MIT"],
+     links: %{
+       "Github" => "http://github.com/minhajuddin/proquint",
+       "Docs"   => "http://hexdocs.pm/proquint",
+     }
   end
 end
